eeb02f402eecf22ae96e125b82130bc892321107
Salomon Smeke <ssmeke@luc.edu>
2017-04-01 22:49:48 -0500

    Main page: first colour choices.

--- js/lib/globals.js
+++ js/lib/globals.js
@@ -1,7 +1,31 @@
 'use strict';
 
 let context = {
-  listeners: {}
+  listeners: {},
+  bar_vals: {}
 };
 
-export{ context }; 
+const INIT_BAR_VALS_OPTIONS = [
+  {
+    x: 'fe4365',
+    p: 0,
+    s: 0.9
+  },
+  {
+    x: '30fd9f',
+    p: 1,
+    s: 1
+  },
+  {
+    x: 'ff5555',
+    p: 0,
+    s: 0.85
+  },
+  {
+    x: 'ff8519',
+    p: 1,
+    s: 0.7
+  }
+];
+
+export{ context, INIT_BAR_VALS_OPTIONS };
