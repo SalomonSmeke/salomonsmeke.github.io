00b50ca13e6c57da9ff9aa590f27e66f86574d75
Salomon Smeke <ssmeke@luc.edu>
2018-09-09 01:20:20 -0500

    Allow minification, fix silly webpack node_env stuff.

--- webpack.config.js
+++ webpack.config.js
@@ -9,75 +9,87 @@ const FILES_DIRS_TO_COPY = [
     'robots.txt', 'README.md', 'LICENSE.txt', 'humans.txt', 'CNAME', 'src/files/',
 ];
 
-module.exports = {
-    devtool: 'eval-source-map',
-    entry: {main: './src/index.js'},
-    output: {
-        filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|dist)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [require('@babel/plugin-proposal-object-rest-spread')],
-                    },
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
+module.exports = (_, {mode}) => {
+    process.env.NODE_ENV = mode;
+    return {
+        devtool: 'eval-source-map',
+        entry: {main: './src/index.js'},
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|dist)/,
+                    use: {
+                        loader: 'babel-loader',
                         options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 100,
-                            },
-                            optipng: {
-                                optimizationLevel: 6,
-                            },
-                            pngquant: {
-                                enabled: false,
-                            },
-                            gifsicle: {
-                                interlaced: true,
-                                optimizationLevel: 3,
-                            },
+                            presets: ['@babel/preset-env'],
+                            plugins: [require('@babel/plugin-proposal-object-rest-spread')],
                         },
                     },
-                ],
-            },
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'file-loader',
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                mozjpeg: {
+                                    progressive: true,
+                                    quality: 100,
+                                },
+                                optipng: {
+                                    optimizationLevel: 6,
+                                },
+                                pngquant: {
+                                    enabled: false,
+                                },
+                                gifsicle: {
+                                    interlaced: true,
+                                    optimizationLevel: 3,
+                                },
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new MiniCssExtractPlugin({
+                filename: 'style.[chunkhash].css',
+            }),
+            new HtmlWebpackPlugin({
+                inject: false,
+                hash: true,
+                minify: mode === 'production' ? [
+                    'collapseWhitespace',
+                    'minifyJS',
+                    'removeComments',
+                    'useShortDoctype',
+                ].reduce((acc, k) => {
+                    acc[k] = true;
+                    return acc;
+                }, {}) : false,
+                template: './src/index.html',
+                filename: 'index.html',
+            }),
+            new CopyWebpackPlugin(
+                FILES_DIRS_TO_COPY
+            ),
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new MiniCssExtractPlugin({
-            filename: 'style.[chunkhash].css',
-        }),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-        new CopyWebpackPlugin(
-            FILES_DIRS_TO_COPY
-        ),
-    ],
+    };
 };
